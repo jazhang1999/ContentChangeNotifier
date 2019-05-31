cc398aefb4a5bbc97d370173b4d6508abdc04ec6
Nick Zhang <jazhang1999@gmail.com>
2019-05-31 11:08:50 -0400

    Added modifications to the ExampleEmailSender java code. Allows for the user to enter in their own email and password, as well as the email of the person they want to send to. More modifications needed in the future

--- ExampleEmailSender.java
+++ ExampleEmailSender.java
@@ -2,34 +2,42 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+import javax.swing.JOptionPane;
 
 public class ExampleEmailSender {
 
     public static void main(String[] args) {
+	
+	//Represents the user's email username and password. Keep in mind that all of these are
+	//case sensitive and will fail if any one of these is spelled incorrectly
+        final String username = JOptionPane.showInputDialog("Username?");
+        final String password = JOptionPane.showInputDialog("Password?");
 
-        final String username = "Sender_Email_Address";
-        final String password = "Sender_Email_Password";
-
+	//Do not touch - sets up the connection between gmail smtp server and computer
         Properties prop = new Properties();
 	prop.put("mail.smtp.host", "smtp.gmail.com");
         prop.put("mail.smtp.port", "587");
         prop.put("mail.smtp.auth", "true");
         prop.put("mail.smtp.starttls.enable", "true"); //TLS
         
+	//Checks to see if the user's credentials are correct. The user must also enable third
+	//party services (done via settings) on their email provider to make this work
         Session session = Session.getInstance(prop,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
                         return new PasswordAuthentication(username, password);
                     }
                 });
-
+	
+	// Sends the message - will be edited to be more varied in the future
         try {
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("Sender_Email_Address"));
+	    final String recipientAddress = JOptionPane.showInputDialog("To who? (Email Address)");
+            message.setFrom(new InternetAddress(username));
             message.setRecipients(
                     Message.RecipientType.TO,
-                    InternetAddress.parse("recipient_email_address")
+                    InternetAddress.parse(recipientAddress)
             );
             message.setSubject("Testing Gmail TLS");
             message.setText("Dear Mail Crawler,"
@@ -39,6 +47,7 @@ public class ExampleEmailSender {
 
             System.out.println("Done");
 
+	//Catches any errors that may occur
         } catch (MessagingException e) {
             e.printStackTrace();
         }
